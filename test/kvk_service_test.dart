2f9f0cffa8f83cafd6af5a3fa5abc0a18679a91d
sietse <svoort1@live.nl>
2022-07-12 15:03:49 +0200

    Added zoeken() tests for all types of testdata

--- test/kvk_service_test.dart
+++ test/kvk_service_test.dart
@@ -7,7 +7,6 @@ import 'package:kvk_service/models/basisprofielen/eigenaar.dart';
 import 'package:kvk_service/models/basisprofielen/vestiging.dart';
 import 'package:kvk_service/models/basisprofielen/vestiging_list.dart';
 import 'package:kvk_service/models/resultaat.dart';
-import 'package:kvk_service/models/resultaat_item.dart';
 import 'package:kvk_service/models/zoek_item.dart';
 
 // TODO: Implementeer tests voor iedere soort testdata
@@ -40,6 +39,182 @@ void main() async {
       () async {
         Resultaat resultaatItems = await service.zoeken(
           ZoekItem(kvkNummer: '90004973'),
+        );
+
+        expect(resultaatItems.runtimeType, Resultaat);
+      },
+    );
+
+    test(
+      'Zoekt naar NV in KvK handelsregister',
+      () async {
+        Resultaat resultaatItems = await service.zoeken(
+          ZoekItem(kvkNummer: '68727720'),
+        );
+
+        expect(resultaatItems.runtimeType, Resultaat);
+      },
+    );
+
+    test(
+      'Zoekt naar NV in KvK handelsregister',
+      () async {
+        Resultaat resultaatItems = await service.zoeken(
+          ZoekItem(kvkNummer: '90004760'),
+        );
+
+        expect(resultaatItems.runtimeType, Resultaat);
+      },
+    );
+
+    test(
+      'Zoekt naar BV in KvK handelsregister',
+      () async {
+        Resultaat resultaatItems = await service.zoeken(
+          ZoekItem(kvkNummer: '68750110'),
+        );
+
+        expect(resultaatItems.runtimeType, Resultaat);
+      },
+    );
+
+    test(
+      'Zoekt naar BV in KvK handelsregister',
+      () async {
+        Resultaat resultaatItems = await service.zoeken(
+          ZoekItem(kvkNummer: '90001354'),
+        );
+
+        expect(resultaatItems.runtimeType, Resultaat);
+      },
+    );
+
+    test(
+      'Zoekt naar Stichting in KvK handelsregister',
+      () async {
+        Resultaat resultaatItems = await service.zoeken(
+          ZoekItem(kvkNummer: '69599068'),
+        );
+
+        expect(resultaatItems.runtimeType, Resultaat);
+      },
+    );
+
+    test(
+      'Zoekt naar Stichting in KvK handelsregister',
+      () async {
+        Resultaat resultaatItems = await service.zoeken(
+          ZoekItem(kvkNummer: '90000102'),
+        );
+
+        expect(resultaatItems.runtimeType, Resultaat);
+      },
+    );
+
+    test(
+      'Zoekt naar VoF in KvK handelsregister',
+      () async {
+        Resultaat resultaatItems = await service.zoeken(
+          ZoekItem(kvkNummer: '69599076'),
+        );
+
+        expect(resultaatItems.runtimeType, Resultaat);
+      },
+    );
+
+    test(
+      'Zoekt naar VoF in KvK handelsregister',
+      () async {
+        Resultaat resultaatItems = await service.zoeken(
+          ZoekItem(kvkNummer: '90005414'),
+        );
+
+        expect(resultaatItems.runtimeType, Resultaat);
+      },
+    );
+
+    test(
+      'Zoekt naar Cooperatie in KvK handelsregister',
+      () async {
+        Resultaat resultaatItems = await service.zoeken(
+          ZoekItem(kvkNummer: '55344526'),
+        );
+
+        expect(resultaatItems.runtimeType, Resultaat);
+      },
+    );
+
+    test(
+      'Zoekt naar Kerkgenootschap in KvK handelsregister',
+      () async {
+        Resultaat resultaatItems = await service.zoeken(
+          ZoekItem(kvkNummer: '90002520'),
+        );
+
+        expect(resultaatItems.runtimeType, Resultaat);
+      },
+    );
+
+    test(
+      'Zoekt naar Onderlinge Waarborg Maatschappij in KvK handelsregister',
+      () async {
+        Resultaat resultaatItems = await service.zoeken(
+          ZoekItem(kvkNummer: '90002490'),
+        );
+
+        expect(resultaatItems.runtimeType, Resultaat);
+      },
+    );
+
+    test(
+      'Zoekt naar Foutmelding in KvK handelsregister',
+      () async {
+        Resultaat resultaatItems = await service.zoeken(
+          ZoekItem(kvkNummer: '90002903'),
+        );
+
+        expect(resultaatItems.runtimeType, Resultaat);
+      },
+    );
+
+    test(
+      'Zoekt naar Vereniging van Eigenaren in KvK handelsregister',
+      () async {
+        Resultaat resultaatItems = await service.zoeken(
+          ZoekItem(kvkNummer: '90000749'),
+        );
+
+        expect(resultaatItems.runtimeType, Resultaat);
+      },
+    );
+
+    test(
+      'Zoekt naar Maatschap in KvK handelsregister',
+      () async {
+        Resultaat resultaatItems = await service.zoeken(
+          ZoekItem(kvkNummer: '90001745'),
+        );
+
+        expect(resultaatItems.runtimeType, Resultaat);
+      },
+    );
+
+    test(
+      'Zoekt naar Commanditaire Vennotschap in KvK handelsregister',
+      () async {
+        Resultaat resultaatItems = await service.zoeken(
+          ZoekItem(kvkNummer: '90003942'),
+        );
+
+        expect(resultaatItems.runtimeType, Resultaat);
+      },
+    );
+
+    test(
+      'Zoekt naar Overige Privaatrechtelijke Rechtspersoon in KvK handelsregister',
+      () async {
+        Resultaat resultaatItems = await service.zoeken(
+          ZoekItem(kvkNummer: '55505201'),
         );
 
         expect(resultaatItems.runtimeType, Resultaat);
